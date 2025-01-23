4add350128d27195c599a8a6f22b4461aef0525c
Davidgmzc <davidgomz.c13@gmail.com>
2025-01-23 22:53:03 +0000

    Terminado por fin

--- src/models.py
+++ src/models.py
@@ -1,53 +1,72 @@
 import os
 import sys
-import enum 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-from sqlalchemy import Enum
 
 Base = declarative_base()
-class MediaType (enum.Enum):
-    image = "Image"
- 
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(250))
-    firstname = Column(String(250))
-    lastname = Column(String(250))
-    email = Column(String(250))
 
-class Post(Base):
-    __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    person = relationship(User)
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(50))
+    apellido = Column(String(50))
+    email = Column(String(100), unique=True)
+    password = Column(String(100))
+    fecha_suscripcion = Column(DateTime)
 
-class Comment(Base):
-    __tablename__ = 'comment' 
-    id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
-    author_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(50), unique=True)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    diameter = Column(Integer)
+    gravity = Column(Integer)
+    terrain = Column(String(50))
+    surface_water = Column(Integer)
+    climate = Column(String(50))
 
-class Media(Base):
-    __tablename__ = 'media'
-    id = Column(Integer, primary_key=True)
-    type = Column(Enum(MediaType), nullable = False)
-    url = Column(String(250))
-    post_id = Column(Integer) 
-    post_id = Column(Integer, ForeignKey('post.id'))
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(50), unique=True)
+    birth_year = Column(Integer)
+    species = Column(String(50))
+    heigth = Column(Integer)
+    mass = Column(Integer)
+    gender = Column(String(50))
+    hair_color = Column(String(50))
+    skin_color = Column(String(50))
+    homeworld = Column(String(50))
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+class Vehicules(Base):
+    __tablename__ = 'vehicules'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(50), unique=True)
+    model = Column(String(50))
+    manufacturer = Column(String(50))
+    clase = Column(String(50))
+    cost = Column(Integer)
+    speed = Column(Integer)
+    length = Column(Integer)
+    cargo_capacity = Column(Integer)
+    mimimum_crew = Column(Integer)
+    passengers= Column(Integer)
 
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    usuario_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    planets_id = Column(Integer, ForeignKey('planets.id'), nullable=False)
+    personaje_id = Column(Integer, ForeignKey('characters.id'), nullable=False)
+    vehicules_id = Column(Integer, ForeignKey('vehicules.id'), nullable=False)
+    fecha_guardado = Column(DateTime)
+
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
